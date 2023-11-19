efb3928b648aacae5a81df5cdb5cb672a6cdab5b
Jack Arnott <140889988+Arnottjwr@users.noreply.github.com>
2023-11-19 14:03:57 +0000

    Update WaveFunctions.py

--- WaveFunctions.py
+++ WaveFunctions.py
@@ -34,11 +34,11 @@ def plucked_string(x,t,L,p,c,a,n):
         x (array-like): Array to plot wave coefficients against 
         t (int): Time at which the string is plucked 
         L (int): Length of the string 
-        v (Float): Location of where the string is plucked 
+        v (int): Location of where the string is plucked 
         c (int): wave speed 
         a (int): Amplitude of initial pluck 
-        n (int): Wave node 
+        n (int): Wave node; Sum for wave superposition at time t 
     """
-
+    # Calculate the Wave Coefficient for the specified parameters 
     A = (2*a*(L**2))*np.sin(n*np.pi*p/L)/((n**2) * (np.pi**2) * p*(L-p))
     return A*np.sin((np.pi*n*x)/L)*np.cos((n*np.pi*c*t)/L)
