e1bdce0b677e8db8f9c8cd240f1baba88207269f
Jack Arnott <140889988+Arnottjwr@users.noreply.github.com>
2024-02-07 16:37:07 +0000

    Update vibrating_string_modes.py

--- Graphic_Codes/vibrating_string_modes.py
+++ Graphic_Codes/vibrating_string_modes.py
@@ -14,23 +14,24 @@ modes = 4
 # String Array 
 x = np.linspace(0, L, 1000)
 
+# Pickups
+neckpickup = 0.275
+bridgepickup = 0.075
+
 fig,ax = plt.subplots(1,figsize=(10, 6))
+ax.get_yaxis().set_visible(False)
 
 for i in range(1, modes + 1):
     modes = string_modes(x, i, L)
-    a1 = np.sin(i* np.pi*0.275)
-    a2 = -np.sin(i* np.pi*0.275)
-    b1 = np.sin(i* np.pi*0.075)
-    b2 = -np.sin(i* np.pi*0.075)
     ax.plot(x, modes + 5/2*i, label=f'n = {i}', color=(0.2 + i/5, 0.4, 0.8)) 
     ax.plot(x, -modes +5/2*i,color=(0.2 + i/5, 0.4, 0.8))
-    ax.plot(([0.275,0.275]),([a1 + 5/2*i,a2 + 5/2*i]),'k',linestyle = '--') #Neck Pickup Line
-    ax.plot(([0.075,0.075]),([b1 + 5/2*i,b2 + 5/2*i]),'k',linestyle = '--') #Bidge Pickup Line
+    a1 = np.sin(i* np.pi*neckpickup)
+    b1 = np.sin(i* np.pi*bridgepickup)
+    ax.plot(([neckpickup,neckpickup]),([a1 + 5/2*i,-a1 + 5/2*i]),'k',linestyle = '--') # Neck Pickup Line
+    ax.plot(([bridgepickup,bridgepickup]),([b1 + 5/2*i,-b1 + 5/2*i]),'k',linestyle = '--') # Bidge Pickup Line
 
-ax.scatter(0.275,1,marker="^",color='b',s=130, zorder=2,label = 'Neck Pickup') # Neck pick-up
-ax.scatter(0.075,1,marker="^",color='r',s=130, zorder=2,label = 'Bridge Pickup') # Neck pick-up
-
+ax.scatter(neckpickup,1,marker="^",color='b',s=130, zorder=2,label = 'Neck Pickup') # Neck pick-up
+ax.scatter(bridgepickup,1,marker="^",color='r',s=130, zorder=2,label = 'Bridge Pickup') # Neck pick-up
 ax.set_title('Individual Vibrating String Modes')
 ax.set_xlabel('String')
-ax.set_ylabel('Amplitude')
 ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
